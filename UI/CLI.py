63286ca59315cd9262bdf709f6701bbb469687eb
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-26 21:07:23 +0200

    Delete UI directory

--- UI/CLI.py
+++
@@ -1,6 +0,0 @@
-# A command line interface allowing terminal users
-# to utilize Qbuild module to search what the users wants
-
-if __name__ == '__main__':
-    # lol there is nothing to do
-    pass
--- UI/GUI.py
+++
@@ -1,3 +0,0 @@
-if __name__ == '__main__':
-    # lol there is nothing to do
-    pass
