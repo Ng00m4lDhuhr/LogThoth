2fb088be0b3a8da5b3cacf20e43313c333f3c540
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-27 17:07:31 +0200

    Update Process.py

--- Timeline/Activity/Process.py
+++ Timeline/Activity/Process.py
@@ -1,20 +1,19 @@
-# Module should take a list of logs and form a timeline of the logs
-# It should be able to return an interactive timeline that is mutable
-# Said time line can be export in different formats for preservation
-# https://dadoverflow.com/2021/08/17/making-timelines-with-python/
+# Module should take a logs and store relevant information in format it in meaningful manner
 
 from datetime import datetime
 from LogThoth.Timeline import Activity
 from LogThoth.Timeline.Event import Logon
 
 
-class Creation(Activity): # Event ID 4688
+# Security Event ID 4688 indicate a creation of process
+class Creation(Activity): 
     def __init__(self, RID: int, timestamp: datetime, file: str, logon: Logon) -> None:
         super().__init__(RID, timestamp)
         self.file = file
         self.logon = logon
         
-class Termination(Activity): # Event ID 4689
+# Security Event ID 4689 indicate a termination of process
+class Termination(Activity): 
     def __init__(self, RID: int, timestamp: datetime, file: str, logon: Logon) -> None:
         super().__init__(RID, timestamp)
         self.file = file
