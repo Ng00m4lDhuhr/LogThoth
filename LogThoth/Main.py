93b10e2bcd832d6fc259d65efcd22adfe1480d28
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-28 00:13:12 +0200

    Create Main.py
    Driver script

---
+++ LogThoth/Main.py
@@ -0,0 +1,10 @@
+# Driver code of the program
+# mainly used for testing and tinkering
+
+if __name__ != '__main__': 
+  print('(!) Script should be ran as main only')
+  exit()
+  
+from LogThoth Import *
+
+# ENTER YOUR SCRIPT HERE
