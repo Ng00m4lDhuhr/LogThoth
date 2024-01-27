a4d022efcf43f3172947d06d094db5c1398a6b52
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-27 16:47:10 +0200

    updated logs dependencies

--- Timeline/Activity/Boot.py
+++ Timeline/Activity/Boot.py
@@ -1,7 +1,7 @@
 from LogThoth.Timeline import Activity
 from datetime import datetime
 
-# System Event ID 32 signals boot type (it was successful) 
+# System Event ID 27 signals boot type (it was successful) 
 class PowerUp(Activity): 
     def __init__(self, RID: int, timestamp: datetime) -> None:
         super().__init__(RID, timestamp)
