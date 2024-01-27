732a70c8e75d64437413db00e16019ce380df2d0
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-27 15:13:19 +0200

    Update Serializer.py

--- Serializer.py
+++ Serializer.py
@@ -1,160 +1,56 @@
 import win32evtlog
-from itertools import chain
-import string
-import time
-import re
-import Timeline
+from Timeline.Event import *
+from enum import Enum
 
-class LogStack(object):
-    
-    LOG_TYPES = ['Setup','System','Security', 'Application']
-    
-    def __init__(self):
-        pass
+class LogType(Enum):
+    SETUP = 'Setup'
+    SYSTEM = 'System'
+    SECURITY = 'Security'
+    APP = 'Application'
 
-    def date2sec(self,evt_date:str) -> int:
-
-        '''
-
-        converts '12/23/99 15:54:09' to seconds
-
-        print '333333',evt_date
-
-        '''
-
-        regexp=re.compile('(.*)\\s(.*)')
-
-        reg_result=regexp.search(evt_date)
-
-        date=reg_result.group(1)
-
-        the_time=reg_result.group(2)
-
-        (mon,day,yr)=map(lambda x: string.atoi(x),string.split(date,'/'))
-
-        (hr,min,sec)=map(lambda x: string.atoi(x),string.split(the_time,':'))
-
-        tup=[yr,mon,day,hr,min,sec,0,0,0]
-
-        sec=time.mktime(tup)
-
-        return sec
-    
-
-    def __repr__(self):
-        return {self.type: self.event}
-    
-    # Union operation of event if types are the same
-    def __add__(self, other): 
-        pass
-    # remove intersected events if types are the same
-    def __sub__(self, other): 
-        pass
-    
-    # logical operators for time comparison
-    # should compare earliest events in the stacks
-    def __eq__(self, other) -> bool:
-        pass
-    def __gt__(self, other) -> bool: 
-        pass
-    def __lt__(self, other) -> bool: 
-        pass
-    def __le__(self, other) -> bool: 
-        pass
-    def __ge__(self, other) -> bool: 
-        pass
-
-    def sort(events:list) -> list:
-        output = []
-        sec = []
+# Get Event By id
+def read_event_logs(EventID:list,log_type:str) -> list:
+    # Open the specified event log
+    handle = win32evtlog.OpenEventLog(None, log_type)
+    # Set the read flags so that it reads first
+    flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
+    # Read all events
+    Events_list = []
+    events = 1
+    while events:
+        events = win32evtlog.ReadEventLog(handle, flags, 0)
         for event in events:
-            sec.append(date2sec(event.TimeGenerated.Format()))
-
-        sorted_sec = sec.sort()
+            if event.EventID in EventID:
+                Events_list.append(event)             
+    # Close the event log
+    win32evtlog.CloseEventLog(handle)
+    return Events_list
         
-        for i in range(len(sorted_sec)):
-            indices = [j for j, x in enumerate(sec) if x == sorted_sec[i]]
-            for index in indices:
-                output.append(events[index])
+######################
+## Query Functions
+######################
 
-    
-# 6005,6006,6013
-                
-    def GetPowerUp(self): # might not be necessary see "GetBootEvent()"
-        return self.read_event_logs([12],"System")
+def GetBootEvents() -> list:
+    Boot = []
+    # TODO : read logs sequentially backwards (old --> future) once as the following (if you see applicable)
+    EventRecords = read_event_logs([12,13],LogType.SYSTEM)
+    if EventRecords[0].id == 13 : EventRecords = EventRecords[1::] # Skips a ShutDown without a previous PowerUp
+    EventRecords = EventRecords[0::-1]  # skips the current PowerUp as we know what is happening rn
+    for up, down in EventRecords : Boot += Boot(up,down)
+    return Boot
 
-    def GetShutDown(self): # might not be necessary see "GetBootEvent()"
-        return self.read_event_logs([13],"System")
+def GetLogonEvents(bootEvent: Event) -> list: 
+    # Query User login in Boot duration
+    # Return list of Logon object
+    pass
 
-    def GetUserLogin(self):
-        return self.read_event_logs([4624],"Security")
-
-    def GetUserLogout(self):
-        return self.read_event_logs([4647],"Security")
-
-    # TODO: yet to be planned as an activity
-    # def GetProcess(self,log_type:str):
-    #     return self.read_event_logs([4688],log_type)
-    
-    # def GetConnection(self):
-    #     return self.read_event_logs(["4624"])
-
-
-    def GetBootEvent(self) -> list:
-        Boot = []
-        # TODO : read logs sequentially backwards (old --> future) once as the following (if you see applicable)
-            # EventRecords = self.read_event_logs([12,13],"System")
-            # if EventRecords[0].id == 13 : EventRecords = EventRecords[1::] # Skips a ShutDown without a previous PowerUp
-            # EventRecords = EventRecords[0::-1]  # skips the current PowerUp as we know what is happening rn
-            # for up, down in EventRecords : Boot += Timeline.Boot(up,down)
-        PowerUp = self.GetPowerUp()
-        ShutDown = self.GetShutDown()
-        for i in range(len(PowerUp)-1):
-
-            Boot.append(Timeline.Boot(PowerUp[i+1],ShutDown(i)))
-        
-        return Boot
-        
-
-    @staticmethod
-    def read_event_logs(EventID:list,log_type:str) -> list:
-        # Open the specified event log
-        handle = win32evtlog.OpenEventLog(None, log_type)
-
-        # Set the read flags so that it reads first
-        flags = win32evtlog.EVENTLOG_BACKWARDS_READ | win32evtlog.EVENTLOG_SEQUENTIAL_READ
-
-        # Read all events
-        Events_list = []
-        events = 1
-        while events:
-            events = win32evtlog.ReadEventLog(handle, flags, 0)
-            for event in events:
-                if event.EventID in EventID:
-                    Events_list.append(event)
-
-            
-            # for event in events:
-            #    print(f"Event Name: {event.EventType}")
-            #    print(f"Time Generated: {event.TimeGenerated.Format()}")
-            #    print(f"Event ID: {event.EventID}")
-            #    print(f"Event Source: {event.SourceName}")
-            #    print(f"Event Category: {event.EventCategory}")
-            #    print(f"Event Data: {event.StringInserts}")
-            #    print("-" * 50)
-
-        # Close the event log
-        win32evtlog.CloseEventLog(handle)
-        return Events_list
-
-
+def GetExecutionEvents(logonEvent: Event) -> list:
+    # Query Processes execution and termination in duration of login
+    # Return list of Binary object
+    pass
 
 if __name__ == "__main__":
-    # Specify the log type [e.g., 'System', 'Security', 'Application']
-    # Logs.read_event_logs('Setup')
-    LogTypes = ['System', 'Security', 'Application','Setup']
-    stack = LogStack()
-    boot = stack.GetBootEvent()
+    boot = GetBootEvents()
 
 
 
