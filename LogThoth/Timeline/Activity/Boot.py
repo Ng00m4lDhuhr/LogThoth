4cffae22c13509f58a029466686c7066615b20d7
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-28 00:02:16 +0200

    Update Boot.py

--- LogThoth/Timeline/Activity/Boot.py
+++ LogThoth/Timeline/Activity/Boot.py
@@ -1,13 +1,19 @@
 from LogThoth.Timeline import Activity
 from datetime import datetime
+from win32apievtlog import PyEventLogRecord
 
 # System Event ID 27 signals boot type (it was successful) 
 class PowerUp(Activity): 
-    def __init__(self, RID: int, timestamp: datetime) -> None:
-        super().__init__(RID, timestamp)
+    def __init__(self, log: PyEventLogRecord) -> None:
+        super().__init__(log)
 
 # System Event ID 1074 indicate process initiated shutdown
-# System Event ID 41 indicate system crash
 class Shutdown(Activity): 
-    def __init__(self, RID: int, timestamp: datetime) -> None:
-        super().__init__(RID, timestamp)
+    def __init__(self, log: PyEventLogRecord) -> None:
+        super().__init__(log)
+
+# TODO : track OS crashes
+# System Event ID 41 indicate system crash
+class Crash(Activity): 
+    def __init__(self, log: PyEventLogRecord) -> None:
+        super().__init__(log)
