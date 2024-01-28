ec3aa9f5a7ae44e126f7cb7d6840ce115fbb37a5
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-28 20:01:57 +0200

    Update Process.py

--- LogThoth/Timeline/Activity/Process.py
+++ LogThoth/Timeline/Activity/Process.py
@@ -3,18 +3,17 @@
 from datetime import datetime
 from LogThoth.Timeline import Activity
 from LogThoth.Timeline.Event import Logon
-from win32apievtlog import PyEventLogRecord
 
 # Security Event ID 4688 indicate a creation of process
 class Creation(Activity): 
-    def __init__(self, log: PyEventLogRecord, logon: Logon) -> None:
+    def __init__(self, log: object, logon: Logon) -> None:
         super().__init__(log)
         self.executable = log.Data   # should find how to parse the data section
         self.logon = logon
         
 # Security Event ID 4689 indicate a termination of process
 class Termination(Activity): 
-    def __init__(self, log: PyEventLogRecord, logon: Logon) -> None:
+    def __init__(self, log: object, logon: Logon) -> None:
         super().__init__(log)
         self.executable = log.Data # should find how to parse the data section
         self.logon = logon 
