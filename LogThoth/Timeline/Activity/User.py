16edc01d41cc959051e55e6a04f91f2ca160c27a
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-01-28 20:02:19 +0200

    Update User.py

--- LogThoth/Timeline/Activity/User.py
+++ LogThoth/Timeline/Activity/User.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 from LogThoth.Timeline import Activity
-from win32apievtlog import PyEventLogRecord
+# from win32apievtlog import PyEventLogRecord
 
 class Login(Activity): # Event ID 4624
-    def __init__(self, log: PyEventLogRecord) -> None:
+    def __init__(self, log: object) -> None:
         super().__init__(log)
         self.logonID = log.Data # should somehow find a way to parse data
         self.SID = log.Data # should somehow find a way ot parse data
@@ -11,7 +11,7 @@ class Login(Activity): # Event ID 4624
   
 class Logout(Activity): # Event ID 4634
     
-    def __init__(self, log: PyEventLogRecord) -> None:
+    def __init__(self, log: object) -> None:
         super().__init__(log)
         self.logonID = log.Data # should somehow find a way to parse data
         self.SID = log.Data # should somehow find a way ot parse data
