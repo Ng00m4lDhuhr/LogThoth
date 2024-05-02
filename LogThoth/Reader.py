5b8154c1c55b9e62b27181aeaec90345a41b3f89
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-05-03 01:57:33 +0300

    Delete LogThoth directory

--- LogThoth/Reader.py
+++
@@ -1,9 +0,0 @@
-import wmi
-
-def get_event_logs(query):
-    api = wmi.WMI()
-    # Query the Win32_NTLogEvent class for events with the specified Event ID
-    event_list = [_ for _ in api.query(query)]
-    return event_list
-
-if __name__ == '__main__': pass
