b59095a9bc8e5442b2891d3c69e496716ca7c3d6
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-06-15 23:11:19 +0300

    Create src/system/windows.py
    a separate file to interact with live windows operating system

---
+++ src/system/windows.py
@@ -0,0 +1,23 @@
+"""
+The purpose of this code is to define default windows values
+and to query said values if have been simply changed
+"""
+
+from sys import argv, stderr
+from os import environ, path
+
+
+# class instance to hold default pathes need for acqusition
+class default(object):
+    SystemRoot : str = "C:\\Windows"
+    LogFolderPath : str = SystemRoot + "\\system32\\winevt\\Logs"
+    SystemLogFilePath : str = LogFolderPath + "\\System.evtx"
+    SecurityLogFilePath : str = LogFolderPath + "\\Security.evtx"
+
+# static class to query live environment
+class query(object):
+
+    @staticmethod
+    def SystemRoot() -> str: return environ['SystemRoot']
+
+    # TODO query each evtx file path from registry key "\\HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\EventLog\\"
