2855b9c817b49bd180a15cb0fcb22e1eef88663f
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-06-16 18:16:44 +0300

    modulization
    making the code into a callable function to allow scripting

--- src/collector.py
+++ src/collector.py
@@ -10,14 +10,23 @@ class IntegrityError(Exception):
     pass
 
 
+def load_records(filepath: str, ignoreIntegrity: bool = False) -> list:
+    # TODO  validate with os.path module https://www.geeksforgeeks.org/os-path-module-python/
+    #       check if it's legit path
+    #       check if it's a file path
+    with Evtx(filepath) as evtx:
+        # TODO  warn user if logs are not authenticated
+        #       evtx.get_file_header()
+        #       if evtx._fh.is_dirty() or not evtx._fh.verify(): raise IntegrityError("Log file has been manipulated")
+        return [ record.lxml() for record in evtx.records() ]
+
+
+# driver/testing code
 if __name__ == '__main__':
-    try: argv[1]
-    except IndexError:
-        print("SYNTAX ERROR: no logs file path was given",file=stderr)
-        exit()
-    with Evtx(windows.default.SecurityLogFilePath) as evtx:
-        # TODO warn user if logs are not authenticated
-        # evtx.get_file_header()
-        # if evtx._fh.is_dirty() or not evtx._fh.verify(): raise IntegrityError("Log file has been manipulated")
-        records = [ record.lxml() for record in evtx.records() ]
-    print(records[-1])
+    try: logsource = argv[1]
+    except IndexError: logsource = windows.default.SecurityLogFilePath
+    try:
+        evtlogs = load_records(filepath=logsource,ignoreIntegrity=False)
+        print(evtlogs[1].find("./System/EventID")) # parsing attempts
+    except KeyboardInterrupt:
+        print("[i] aborted by user")
