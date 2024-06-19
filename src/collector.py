4a31531e04ccd35b6a2c7cd8edb5d76fef66b3ef
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-06-19 13:40:02 +0300

    better application of functional programming
    - renamed load_records function to load_file_records
    - made the code act more as a module
    - test part is still functional
    - added integrity checks of evtx log file

--- src/collector.py
+++ src/collector.py
@@ -1,6 +1,4 @@
 from Evtx.Evtx import Evtx
-from sys import argv, stderr
-from system import windows
 
 
 class IntegrityError(Exception):
@@ -10,23 +8,27 @@ class IntegrityError(Exception):
     pass
 
 
-def load_records(filepath: str, ignoreIntegrity: bool = False) -> list:
+def load_file_records(filepath: str, ignoreIntegrity: bool = False) -> list:
     # TODO  validate with os.path module https://www.geeksforgeeks.org/os-path-module-python/
     #       check if it's legit path
     #       check if it's a file path
     with Evtx(filepath) as evtx:
-        # TODO  warn user if logs are not authenticated
-        #       evtx.get_file_header()
-        #       if evtx._fh.is_dirty() or not evtx._fh.verify(): raise IntegrityError("Log file has been manipulated")
+        # TODO  warn user about logs integrity
+        if not ignoreIntegrity:
+            evtx.get_file_header()
+            if evtx._fh.is_dirty() or not evtx._fh.verify():
+                raise IntegrityError("Log file has been manipulated")
         return [ record.lxml() for record in evtx.records() ]
 
 
 # driver/testing code
 if __name__ == '__main__':
+    from sys import argv, stderr
+    from system import windows
     try: logsource = argv[1]
     except IndexError: logsource = windows.default.SecurityLogFilePath
     try:
-        evtlogs = load_records(filepath=logsource,ignoreIntegrity=False)
+        evtlogs = load_file_records(filepath=logsource,ignoreIntegrity=True)
         print(evtlogs[1].find("./System/EventID")) # parsing attempts
     except KeyboardInterrupt:
-        print("[i] aborted by user")
+        print("(i) aborted by user", file=stderr)
