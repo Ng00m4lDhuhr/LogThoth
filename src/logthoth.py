bd697e516429e1a3a9cb8e963f0f379623c49269
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-06-19 19:27:24 +0300

    Multiple log type records loading
    - Automated record files path recognition
    - Removed direct usage of modules in the main function
    - bug fixes

--- src/logthoth.py
+++ src/logthoth.py
@@ -1,16 +1,26 @@
-#!/usr/bin/python
+#!/user/bin/python3
 import collector
 from sys import argv, stderr
 from system import windows
 
-def load_security_records (filepath:str=None) -> list: pass
-def load_system_records   (filepath:str=None) -> list: pass
+def load_security_records (filepath:str=None) -> list:
+    if filepath == None:
+        filepath = windows.default.SecurityLogFilePath
+    return collector.load_file_records(filepath=filepath,ignoreIntegrity=True)
+
+def load_system_records   (filepath:str=None) -> list:
+    if filepath == None:
+        filepath = windows.default.SystemLogFilePath
+    return collector.load_file_records(filepath=filepath,ignoreIntegrity=True)
+
 
 if __name__ == '__main__':
-    try: logsource = argv[1]
-    except IndexError: logsource = windows.default.SecurityLogFilePath
     try:
-        evtlogs = collector.load_file_records(filepath=logsource,ignoreIntegrity=True)
-        print(evtlogs[1].find("./System/EventID")) # parsing attempts
+        evtlogs = {}
+        evtlogs["security"] = load_security_records()
+        evtlogs["system"] = load_system_records()
+        print(evtlogs["security"].find("./System/EventID")) # parsing attempts
+        print(evtlogs["system"].find("./System/EventID"))   # parsing attempts
     except KeyboardInterrupt:
         print("[i] aborted by user", file=stderr)
+        quit()
