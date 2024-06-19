d9f59db6ae34bfa25cf225789aeb298b2e59f3ac
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-06-19 13:41:02 +0300

    added basic driver

--- src/logthoth.py
+++ src/logthoth.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python
+import collector
+from sys import argv, stderr
+from system import windows
+
+def load_security_records (filepath:str=None) -> list: pass
+def load_system_records   (filepath:str=None) -> list: pass
+
+if __name__ == '__main__':
+    try: logsource = argv[1]
+    except IndexError: logsource = windows.default.SecurityLogFilePath
+    try:
+        evtlogs = collector.load_file_records(filepath=logsource,ignoreIntegrity=True)
+        print(evtlogs[1].find("./System/EventID")) # parsing attempts
+    except KeyboardInterrupt:
+        print("[i] aborted by user", file=stderr)
