3b97b46a440e55f20c69a8a4649549979bdf1923
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-06-20 13:58:10 +0300

    read log files (#4)
    * Delete src/plaso directory
    * Create collector.py
    * basic access to log file, no parsing yet
    * update python requirements
    * Added shebang line
    * parsable logs output
    * added lxml logs reading capabilities
    * moved windows environment and default values to [separate file](https://github.com/Ng00m4lDhuhr/LogThoth/src/system/windows.py)
    * loadable records
    * better application of functional programming
    * added integrity checks of evtx log file
    * added basic testing driver code
    * Multiple log type records loading
    * Automated record files path recognition
    * Removed direct usage of modules in the main function
    * bug fixes
    * better display of record reading results
    * access to windows global variable %COMPUTERNAME%

---
+++ src/collector.py
@@ -0,0 +1,34 @@
+from Evtx.Evtx import Evtx
+
+
+class IntegrityError(Exception):
+    """
+    class to handls log files integrity errors
+    """
+    pass
+
+
+def load_file_records(filepath: str, ignoreIntegrity: bool = False) -> list:
+    # TODO  validate with os.path module https://www.geeksforgeeks.org/os-path-module-python/
+    #       check if it's legit path
+    #       check if it's a file path
+    with Evtx(filepath) as evtx:
+        # TODO  warn user about logs integrity
+        if not ignoreIntegrity:
+            evtx.get_file_header()
+            if evtx._fh.is_dirty() or not evtx._fh.verify():
+                raise IntegrityError("Log file has been manipulated")
+        return [ record.lxml() for record in evtx.records() ]
+
+
+# driver/testing code
+if __name__ == '__main__':
+    from sys import argv, stderr
+    from system import windows
+    try: logsource = argv[1]
+    except IndexError: logsource = windows.default.SecurityLogFilePath
+    try:
+        evtlogs = load_file_records(filepath=logsource,ignoreIntegrity=True)
+        print(evtlogs[1].find("./System/EventID")) # parsing attempts
+    except KeyboardInterrupt:
+        print("(i) aborted by user", file=stderr)
--- src/logthoth.py
+++ src/logthoth.py
@@ -1,1 +1,26 @@
-from plaso import parsers
+#!/user/bin/python3
+import collector
+from sys import argv, stderr
+from system import windows
+
+def load_security_records (filepath:str=None) -> list:
+    if filepath == None:
+        filepath = windows.default.SecurityLogFilePath
+    return collector.load_file_records(filepath=filepath,ignoreIntegrity=True)
+
+def load_system_records   (filepath:str=None) -> list:
+    if filepath == None:
+        filepath = windows.default.SystemLogFilePath
+    return collector.load_file_records(filepath=filepath,ignoreIntegrity=True)
+
+
+if __name__ == '__main__':
+    try:
+        evtlogs = {}
+        evtlogs["security"] = load_security_records()
+        evtlogs["system"] = load_system_records()
+        print("Security Records:", len(evtlogs["security"]) ) 
+        print("System Records:", len(evtlogs["system"]) )  
+    except KeyboardInterrupt:
+        print("[i] aborted by user", file=stderr)
+        quit()
