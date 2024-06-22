5df964ae359e411af28c502a48f879297f89fc6c
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2024-06-22 22:21:47 +0300

    file path assertion
    based on @Zeeyad-Sayed  code on [this commit(https://github.com/Ng00m4lDhuhr/LogThoth/blob/fb33f00e95f367c7cd3cbd2bc389d9820553119b/src/collector.py#L11)

--- src/collector.py
+++ src/collector.py
@@ -7,17 +7,25 @@ class IntegrityError(Exception):
     """
     pass
 
+def assert_file_path(filepath: str) -> bool:
+    """
+    function to assert if the provided path is a legitimate file path.
+    """
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"The file {filepath} does not exist.")
+    if not os.path.isfile(filepath):
+        raise ValueError(f"The path {filepath} is not a file.")
+    return True
+
 
 def load_file_records(filepath: str, ignoreIntegrity: bool = False) -> list:
-    # TODO  validate with os.path module https://www.geeksforgeeks.org/os-path-module-python/
-    #       check if it's legit path
-    #       check if it's a file path
+    assert_file_path(filepath)
     with Evtx(filepath) as evtx:
         # TODO  warn user about logs integrity
         if not ignoreIntegrity:
             evtx.get_file_header()
             if evtx._fh.is_dirty() or not evtx._fh.verify():
-                raise IntegrityError("Log file has been manipulated")
+                raise IntegrityError("None trusted log source")
         return [ record.lxml() for record in evtx.records() ]
 
 
