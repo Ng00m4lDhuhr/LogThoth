6c5479b9855a2d28d5a6798a81052f4253c85cd5
DeadDude-glitch <ahmed.ussef248@gmail.com>
2024-06-27 01:13:36 +0300

    more intutive code

--- src/interface/log.py
+++ src/interface/log.py
@@ -60,7 +60,54 @@ class event(object):
         except KeyError: return None
 
 
-class evt4624(event):
+class _logon(event):
+    """
+    class to ease access to EventData of logon attempts 
+    """
+
+    def __str__(self) -> str:
+        return f"<machine={self.computer}/user={self.username} logonid={self.id} type={self.type}>"
+
+    @property
+    def username(self) -> str:
+        """The target username property."""
+        return self.data("TargetUserName")
+    
+    @property
+    def logonType(self) -> (int,str):
+        """The LogonType property."""
+        return int(self.data("LogonType"))   
+    
+    @property
+    def logonID(self) -> int:
+        """The target logon id property."""
+        return int(self.data("TargetLogonId"), 16)
+
+    @property
+    def process(self) -> str:
+        """the process authenticating a user"""
+        return self.data("ProcessName")
+
+    @property
+    def ip(self) -> str:
+        """source ip address"""
+        return self.data("IpAddress")
+
+    @property
+    def port(self) -> str:
+        """source ip port"""
+        return self.data("IpPort")
+
+    @property
+    def pid(self) -> int: 
+        """the process id authenticating as user"""
+        return int(self.data("ProcessId"), 16)
+
+
+
+
+
+class evt4624(_logon):
     """
     class to ease access to EventData of successful logon attempts 
     see https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/event-4624
@@ -70,77 +117,18 @@ class evt4624(event):
         super().__init__(record)
         if self.id != 4624: raise ValueError(f"Unexpected Event: given EventId is {self.id} expected 4624")
 
-    def __str__(self) -> str:
-        return f"<machine={self.computer}/user={self.username} logonid={self.id} type={self.type}>"
 
-    @property
-    def username(self) -> str:
-        """The target username property."""
-        return self.data("TargetUserName")
-    
-    @property
-    def logonType(self) -> (int,str):
-        """The LogonType property."""
-        return int(self.data("LogonType"))   
-    
-    @property
-    def logonID(self) -> int:
-        """The target logon id property."""
-        return int(self.data("TargetLogonId"), 16)
-
-    @property
-    def process(self) -> str:
-        """the process authenticating a user"""
-        return self.data("ProcessName")
-
-    @property
-    def pid(self) -> int: 
-        """the process id authenticating as user"""
-        return int(self.data("ProcessId"), 16)
-
-
-
-class evt4625(event):
+class evt4625(_logon):
     """
     a class to ease access to EventData of failed logon attempts
     see https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-10/security/threat-protection/auditing/event-4625
     """
 
-
     def __init__(self, record:object):
         super().__init__(record)
         if self.id != 4625: 
             raise ValueError(f"Unexpected Event: given EventId is {self.id} expected 4625")
-
-    def __str__(self) -> str:
-        return f"<machine={self.computer}/user={self.username} logonid={self.id} type={self.type}>"
-
-
-    @property
-    def username(self) -> str:
-        """The target username property."""
-        return self.data("TargetUserName")
-    
-    @property
-    def logonType(self) -> (int,str):
-        """The LogonType property."""
-        return int(self.data("LogonType"))   
         
-    @property
-    def logonID(self) -> int:
-        """The target logon id property."""
-        return int(self.data("TargetLogonId"), 16)
-
-    @property
-    def process(self) -> str:
-        """the process authenticating a user"""
-        return self.data("ProcessName")
-
-    @property
-    def processID(self) -> int: 
-        """the process id authenticating as user"""
-        return int(self.data("ProcessId"), 16)
-
     @property
     def status(self, string=False) -> (int, str):
        """substatus translation of failure reason"""
@@ -151,13 +139,14 @@ class evt4625(event):
         """substatus translation of failure reason"""
         return int( self.data(SubStatus), 16 )
 
+
 def classify(record:object) -> event:
     # TODO
     """ function that decides the type of a log entry """
     if   4624 == int(record.find(".//e:EventID", namespaces=event.ns).text): return evt4624(record)
     elif 4625 == int(record.find(".//e:EventID", namespaces=event.ns).text): return evt4625(record)
-    # elif 4688 == int(record.find(".//e:EventID", namespaces=self.ns).text): pass
-    # elif 4689 == int(record.find(".//e:EventID", namespaces=self.ns).text): pass
+    # elif 4688 == int(record.find(".//e:EventID", namespaces=event.ns).text): pass
+    # elif 4689 == int(record.find(".//e:EventID", namespaces=event.ns).text): pass
     else: return event(record) # idk idc
 
 
