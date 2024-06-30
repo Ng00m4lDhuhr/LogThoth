dc696c4cbaf943f8155cef272f3197b5e0101edd
DeadDude-glitch <ahmed.ussef248@gmail.com>
2024-06-30 21:58:33 +0300

    incremental scope id

---
+++ src/timeline/identifier.py
@@ -0,0 +1,27 @@
+"""
+Each event log has enough info to understand the context of the record
+This code should provide a container that encompasses all the logs within a defined context
+"""
+
+
+class context(object):
+    """virtual class to define contexts"""
+    pass
+
+
+class session(context):
+    """object to identify a session"""
+    def __init__(self, sid:str, host:str, logonId:int):
+        self.logonId = int(logonId) # live unique session id
+        self.sid = str(sid)         # user identifier
+        self.host = str(host)       # hosting machine identifier
+
+
+class execution(context):
+    """object to identify a program execution"""
+    def __init__(self,  pid:int, image:str, session:session):
+        self.session = session 
+        self.image = str(image)
+        self.pid = int(pid)
+
+
