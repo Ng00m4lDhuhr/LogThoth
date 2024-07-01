5f7a4dc026b2527676f3d8d08324fbcf7c06ec3b
Zeyad <88353811+Zeeyad-Sayed@users.noreply.github.com>
2024-07-01 09:38:44 +0300

    Added extra methods to determine the elapsed timings for different parts of the code to logthoth.py
    Added extra time calculations for: - collection phase, - parsing phase, - fetching a sample

--- src/logthoth.py
+++ src/logthoth.py
@@ -27,29 +27,33 @@ if __name__ == '__main__':
     start_time = time.time() #Start the timer
     try:
         evtlogs = {}
-        
+        collection_start_time = time.time()
         print("(~) collection phase...", end='\r')
         try:
             evtlogs["security"] = load_security_records()
             print("(i) collected security records:", len(evtlogs["security"]))
 
         except Exception as e:
-            print("(!) cannot parse security file:",e, file=stderr)
+            print("(!) cannot collect security file:",e, file=stderr)
           
         try:
             evtlogs["system"] = load_system_records()
             print("System Records:", len(evtlogs["system"]) )
         except Exception as e:
-            print("(!) cannot parse system file:",e, file=stderr)
-          
+            print("(!) cannot collect system file:",e, file=stderr)
+        collection_end_time = time.time()
+
+        parsing_start_time = time.time()
         print("(~) parsing phase...", end='\r')
         try:
             for event in evtlogs["security"]: event = log.classify(event)
             print("(i) parsing phase...done") 
         except Exception as e:
             print("(!) cannot parse security logs:", e, file=stderr)
-        
+        parse_end_time = time.time()
+      
         # print a sample record to check parsing
+        fetching_start_time = time.time()
         try:
             if evtlogs["security"]:
                 print("Sample Security Record:", evtlogs["security"][0])
@@ -65,6 +69,17 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("(i) aborted by user", file=stderr)
         quit()
+    fetch_end_time = time.time() 
     end_time = time.time() # End the timer
-    elapsed_time = end_time - start_time # Calculate elapsed time
-    print(f"Total execution time: {elapsed_time: .2f} seconds")
+    
+    # Calculate elapsed times
+    total_elapsed_time = end_time - start_time
+    collection_time = collection_end_time - collection_start_time
+    parsing_time = parsing_end_time - parsing_start_time
+    fetching_time = fetching_end_time - fetching_start_time
+
+    # Print the timings
+    print(f"Collection time: {collection_time:.2f} seconds")
+    print(f"Parsing time: {parsing_time:.2f} seconds")
+    print(f"Fetching sample time: {fetching_time:.2f} seconds")
+    print(f"Total execution time: {total_elapsed_time: .2f} seconds")
